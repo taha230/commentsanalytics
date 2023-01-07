a13fbf4e69f5a87db78c6f86492620c6a0aa1b89
taha230 <taha_hamedani@yahoo.com>
2023-01-07 15:55:50 +0330

    chrome extention

--- chrome-extention/Youtube_Comment_Scraper/content/content.js
+++ chrome-extention/Youtube_Comment_Scraper/content/content.js
@@ -13022,7 +13022,7 @@
             value: function() {
                 if (l) {
                     $("#player").remove(), $("#secondary-inner").remove(), $("#related").remove();
-                    $("#masthead-container").append('\n      <div style="text-align: center; color: white; padding: 10px; background: #5c5eda; font-size: 14px;">\n        <p>COMMENTS ANALYTICS SCRAPER IS WORKING, PLEASE DO NOT CLOSE THE WINDOW !!!</p>\n      </div>\n      \n    ');
+                    $("#masthead-container").append('\n      <div style="text-align: center; color: white; padding: 10px; background: #5c5eda; font-size: 14px;">\n        <p>COMMENTS ANALYTICS SCRAPER IS WORKING, PLEASE DO NOT CLOSE THE WINDOW.</p>\n      </div>\n      <div style="text-align: center; padding: 10px; background: white; font-size: 14px;">\n        <p>\n          <button id="dl_btn" style="color: white; padding: 5px; background: #6977ae; font-size: 14px; border: none;">DOWNLOAD COMMENTS <span id="c_count"></span></button>\n        </p>\n      </div>\n    ');
                     var t = this;
                     this.scrollInterval = setInterval(function() {
                         t.scrollDown()
@@ -13048,8 +13048,9 @@
                 e = $("ytd-app").height();
                 var n = t || "slow";
                 // $("#masthead-container").append('\n      <div style="text-align: center; color: white; padding: 10px; background: #5c5eda; font-size: 14px;">\n        <p>Down </p>\n      </div>\n      \n    ');
-                $("#masthead-container").append(e);
-
+                // $("#masthead-container").append(e);
+                $("#c_count").text("( " + g.comments.length + " )")
+                
                 return new Promise(function(t) {
                     $("html,body").animate({
                         scrollTop: e
