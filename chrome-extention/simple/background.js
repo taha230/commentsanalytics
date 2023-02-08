f16ade1086e5782b58b1e9eadb757e6a3f03ca47
taha230 <taha_hamedani@yahoo.com>
2023-02-08 16:21:02 +0330

    add simple chrom extension

--- chrome-extention/simple/background.js
+++ chrome-extention/simple/background.js
@@ -1,6 +1,22 @@
-let contextMenuItem = {
-    "id": "workTime",
-    "title": "WorkTime",
-    "contexts": ["selection"]
-    };
-    chrome.contextMenus.create(contextMenuItem);
+const CONTEXT_MENU_ID = "EXPORT_COMMENT";
+
+function exportComment(info, tab) {
+  if (info.menuItemId === CONTEXT_MENU_ID) {
+    chrome.tabs.executeScript({
+      file: "jquery-3.4.1.min.js"
+    }, function() {
+      chrome.tabs.executeScript({
+        file: "content.js"
+      });
+    });
+  }
+}
+
+chrome.contextMenus.create({
+  title: "Export Comments",
+  documentUrlPatterns: ["*://www.youtube.com/watch?*"],
+  contexts: ["all"],
+  id: CONTEXT_MENU_ID
+});
+
+chrome.contextMenus.onClicked.addListener(exportComment);
