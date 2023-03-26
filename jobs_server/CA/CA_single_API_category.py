b7bb887e0b81fe2cbe202cc5316a2986bdb49530
taha230 <taha_hamedani@yahoo.com>
2023-03-26 13:15:48 +0330

    corrected use-multiprocessing

--- jobs_server/CA/CA_single_API_category.py
+++ jobs_server/CA/CA_single_API_category.py
@@ -21,8 +21,8 @@ import yarl
 import bson
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from transformers import pipeline
-import spacy
-from keybert import KeyBERT
+# import spacy
+# from keybert import KeyBERT
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from simpletransformers.classification import ClassificationModel
@@ -38,10 +38,28 @@ transformers_logger.setLevel(logging.WARNING)
 
 model_path = "outputs/model_args.json"
 
-model_youtube = ClassificationModel(
-"roberta", "outputs", use_cuda=False, args={'reprocess_input_data': True,
+
+# model_youtube = ClassificationModel(
+# "roberta", "outputs", use_cuda=False, args={'reprocess_input_data': True,
+#                                   'overwrite_output_dir': True,
+#                                   'use_multiprocessing': False,
+#                                   'num_train_epochs': 10,
+#                                   'learning_rate': 3e-5,
+#                                   'train_batch_size': 4,
+#                                   'eval_batch_size': 1,
+#                                   'gradient_accumulation_steps': 1,
+#                                   'save_steps': 1000,
+#                                   'fp16': False}
+# )
+
+model_youtube = None
+
+def get_model():
+    global model_youtube
+    if model_youtube is None:
+        model_args = args={'reprocess_input_data': True,
                                   'overwrite_output_dir': True,
-                                  'use_multiprocessing': False,
+                                  "use_multiprocessing": False, "use_multiprocessing_for_evaluation": False, "process_count": 1,
                                   'num_train_epochs': 10,
                                   'learning_rate': 3e-5,
                                   'train_batch_size': 4,
@@ -49,7 +67,9 @@ model_youtube = ClassificationModel(
                                   'gradient_accumulation_steps': 1,
                                   'save_steps': 1000,
                                   'fp16': False}
-)
+        model_youtube = ClassificationModel('roberta', "outputs", use_cuda=False, args=model_args)
+    return model_youtube
+
 
 youtube_categories_json = {
     'Feedback' : 0,
@@ -69,8 +89,8 @@ application.config["DEBUG"] = True
 def youtube_category_extraction(text):
     try:
 
-
-        predictions_youtube, raw_outputs_youtube = model_youtube.predict([text])
+        cl_model = get_model()
+        predictions_youtube, raw_outputs_youtube = cl_model.predict([text])
         return [list(youtube_categories_json)[predictions_youtube[0]]]
 
     except Exception as e:
@@ -95,7 +115,7 @@ def start_CA_single():
         if (request_type == 'Sentiment Analysis'):
             json_out['result'] = 'Neutral'
         else:
-            json_out['result'] = str(['Other'])
+            json_out['result'] = ['Others']
 
         return json_out
 
@@ -106,7 +126,7 @@ def start_CA_single():
         if (request_type == 'Sentiment Analysis'):
             json_out['result'] = 'Neutral'
         else:
-            json_out['result'] = str(['Other'])
+            json_out['result'] = ['Others']
 
         return json_out
 
