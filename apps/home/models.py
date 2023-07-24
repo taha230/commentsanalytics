5548770dbeb11a74f76d4771977f4b88549cf7f5
taha230 <taha_hamedani@yahoo.com>
2023-07-24 15:00:08 +0330

    change freemium credit to 50

--- apps/home/models.py
+++ apps/home/models.py
@@ -226,7 +226,7 @@ class User_Log(models.Model):
 
 class User_Other_Fields(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    remain_count = models.IntegerField(blank=True, default=10)
+    remain_count = models.IntegerField(blank=True, default=50)
     expired_date = models.DateTimeField(blank=True, null=True)
     promotion_code_used = models.BooleanField(blank=True, null=True, default=False)
     user_key = models.CharField(max_length=50, null=True)
